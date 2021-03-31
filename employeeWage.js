c3621364f845346313cda90dcf7ad695b35caf9c
amitkrmaharana <ak657harley@gmail.com>
2021-03-31 21:06:34 +0530

    Refactored: Checked if employee is absent or present

--- employeeWage.js
+++ employeeWage.js
@@ -1,1 +1,9 @@
 console.log("Welcome To Employee Wage Computation program");
+const IS_ABSENT = 0;
+let checkEmp = Math.floor(Math.random() * 10) % 2;
+if (checkEmp == 0) {
+    console.log("Employee is Absent");
+    return;
+}
+else 
+    console.log("Employee is Present");
