a652aef8f41f206a673a4deece9a0b4ad05665ae
amitkrmaharana <ak657harley@gmail.com>
2021-03-31 22:57:42 +0530

    Refactored: created a afunction to get working hours

--- employeeWage.js
+++ employeeWage.js
@@ -1,33 +1,22 @@
-console.log("Welcome To Employee Wage Computation program");
-{
-    const IS_ABSENT = 0;
-    let checkEmp = Math.floor(Math.random() * 10) % 2;
-    if (checkEmp == 0) {
-        console.log("Employee is Absent");
-        return;
-    }
-    else 
-        console.log("Employee is Present");
-}
-
 const IS_FULL_TIME = 1;
 const IS_PART_TIME = 2;
 const FULL_TIME_HOUR = 8;
 const PART_TIME_HOUR = 4;
 const WAGE_PER_HOUR = 20;
 
+
+function getWorkingHours(empCheck) {
+    switch(checkEmp) {
+        case IS_FULL_TIME:
+            return FULL_TIME_HOUR;
+        case IS_PART_TIME:
+            return PART_TIME_HOUR;
+        default:
+            return 0;
+    }
+}
 let empHr = 0;
 let checkEmp = Math.floor(Math.random() * 10) % 3;
-switch(checkEmp) {
-    case IS_FULL_TIME:
-        empHr = FULL_TIME_HOUR;
-        break;
-    case IS_PART_TIME:
-        empHr = PART_TIME_HOUR;
-        break;
-    default:
-        empHr = 0;
-}
-
+empHr = getWorkingHours(checkEmp);
 let dailyEmployeeWage = empHr * WAGE_PER_HOUR;
-console.log("Daily Wage is " + dailyEmployeeWage);
+console.log("Employee Hours: " + empHr + " Daily Wage: " + dailyEmployeeWage);
