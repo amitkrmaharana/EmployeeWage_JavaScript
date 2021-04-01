d05ad75122606d3d5914f32a4dbc8b5453f190dd
amitkrmaharana <ak657harley@gmail.com>
2021-04-01 06:45:27 +0530

    Refactored: Stored the daily wage in an array

--- employeeWage.js
+++ employeeWage.js
@@ -17,12 +17,22 @@ function getWorkingHours(empCheck) {
             return 0;
     }
 }
+
+function calculateDailyWage(empHr) {
+    return empHr * WAGE_PER_HOUR;
+}
+
 let totalempHr = 0;
 let totalWorkingDays = 0;
+let employeeDailyWagesArr = new Array();
+
 while (totalempHr < MAX_WORKING_HOUR && totalWorkingDays < MAX_WORKING_DAYS) {
     let checkEmp = Math.floor(Math.random() * 10) % 3;
-    totalempHr += getWorkingHours(checkEmp);
+    let empHr = getWorkingHours(checkEmp);
+    totalempHr += empHr
+    employeeDailyWagesArr.push(calculateDailyWage(empHr));
     totalWorkingDays++;
 }
 let monthlyWage = totalempHr * WAGE_PER_HOUR;
 console.log("Total Employee Hours: " + totalempHr + "\nTotal Working Days: " + totalWorkingDays + "\nMonthly Wage: " + monthlyWage);
+console.log("Daily Wages are: \n" + employeeDailyWagesArr);
