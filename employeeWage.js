6a1f5d46e46c28d2da281c1b6ed5c283c54ccd38
amitkrmaharana <ak657harley@gmail.com>
2021-04-02 05:21:01 +0530

    Refactored: Used object operation by arrow functions

--- employeeWage.js
+++ employeeWage.js
@@ -17,31 +17,53 @@ function getWorkingHours(empCheck) {
             return 0;
     }
 }
-
 function calculateDailyWage(empHr) {
     return empHr * WAGE_PER_HOUR;
 }
 let totalempHr = 0;
 let totalWorkingDays = 0;
-let employeeDailyWagesArr = new Array();
+let employeeDailyHrsAndWagesArr = new Array();
 while (totalempHr < MAX_WORKING_HOUR && totalWorkingDays < MAX_WORKING_DAYS) {
     let checkEmp = Math.floor(Math.random() * 10) % 3;
     let empHr = getWorkingHours(checkEmp);
     totalempHr += empHr
     totalWorkingDays++;
-    employeeDailyWagesArr.push(
+    employeeDailyHrsAndWagesArr.push(
         {
-            dayNum:totalWorkingDays,
-            dailyHours:empHr,
-            dailyWage:calculateDailyWage(empHr),
+            dayNum: totalWorkingDays,
+            dailyHours: empHr,
+            dailyWage: calculateDailyWage(empHr),
             toString() {
                 return '\nDay' + this.dayNum + ' => Working Hours is ' + this.dailyHours +
-                        ' And Wage Earned = ' + this.dailyWage
+                    ' And Wage Earned = ' + this.dailyWage
             },
 
         }
     );
 }
+console.log("Daily Hours Worked and Wage Earned: " + employeeDailyHrsAndWagesArr);
 
-console.log("Daily Hours Worked and Wage Earned: " + employeeDailyWagesArr);
+let totalWages = employeeDailyHrsAndWagesArr
+    .filter(dailHrsAndWage => dailHrsAndWage.dailyWage > 0)
+    .reduce((totalWage, dailHrsAndWage) => totalWage += dailHrsAndWage.dailyWage, 0);
 
+let totalHours = employeeDailyHrsAndWagesArr
+    .filter(dailHrsAndWage => dailHrsAndWage.dailyWage > 0)
+    .reduce((totalWage, dailHrsAndWage) => totalWage += dailHrsAndWage.dailyHours, 0);
+
+console.log("Total Hours: " + totalHours + " Total Wages: " + totalWages);
+
+employeeDailyHrsAndWagesArr.filter(dailHrsAndWage => dailHrsAndWage.dailyHours == 8)
+    .forEach(dailyHrsAndWage => console.log(dailyHrsAndWage.toString()));
+
+let partWorkingDaysStrArr = employeeDailyHrsAndWagesArr
+    .filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours == 4)
+    .map(dailHrsAndWage => dailHrsAndWage.toString());
+
+console.log("Part Working Day Strings: " + partWorkingDaysStrArr);
+
+let nonWorkingDaysStrArr = employeeDailyHrsAndWagesArr
+    .filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours == 0)
+    .map(dailHrsAndWage => dailHrsAndWage.dayNum);
+
+console.log("Non Working Day Strings: " + nonWorkingDaysStrArr);
